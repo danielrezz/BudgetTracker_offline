82ca75796c2aa2c276fff1d5fd5f0ef5f715c698
Daniel Reza <daniel.reza1007@gmail.com>
2021-03-06 14:45:34 -0600

    deploying

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.PORT || "mongodb://localhost/budget", 
+  process.env.PORT || 3000 || "mongodb://localhost/budget", 
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
