9957a307ceac0a719c43d53a22bc0ca1d4a2be84
Daniel Reza <daniel.reza1007@gmail.com>
2021-03-06 14:33:03 -0600

    replacing PORT with MONGODB_URI in server file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const MONGODB_URI = 3000;
 
 const app = express();
 
@@ -27,6 +27,6 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(MONGODB_URI, () => {
+  console.log(`App running on port ${MONGODB_URI}!`);
 });
